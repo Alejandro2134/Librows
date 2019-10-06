d0608e60186718626006f40fea3c734cad59fff4
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2019-10-06 14:45:00 -0500

    Config

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 //Inicialización de modulos
 const app = express();
 
 //Settings
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main', 
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
 
 //Middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //Variables globales
+app.use((req, res, next ) => {
+    next();
+});
 
 //Routes
+app.use(require('./routes'));
 
 //Public
 
