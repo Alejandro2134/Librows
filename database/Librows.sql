7edc14e511ceef4b471f1574cd157bce9bc0ffc2
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2019-11-17 15:46:39 -0500

    Database

---
+++ database/Librows.sql
@@ -0,0 +1,282 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 17-11-2019 a las 21:43:35
+-- Versión del servidor: 10.3.15-MariaDB
+-- Versión de PHP: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `Librows`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `calificacion`
+--
+
+CREATE TABLE `calificacion` (
+  `idCalificacion` int(200) NOT NULL,
+  `idComentarioFK` int(11) DEFAULT NULL,
+  `calificacion` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `calificacionTotal`
+--
+
+CREATE TABLE `calificacionTotal` (
+  `idCalificacionTotal` int(200) NOT NULL,
+  `idLibroFK` int(11) DEFAULT NULL,
+  `calificacionT` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `idComentario` int(255) NOT NULL,
+  `idLibroFK` int(11) DEFAULT NULL,
+  `idUsuarioFK` int(11) DEFAULT NULL,
+  `descripcion` varchar(200) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libro`
+--
+
+CREATE TABLE `libro` (
+  `idLibro` int(100) NOT NULL,
+  `nombre` varchar(180) COLLATE utf8_spanish_ci NOT NULL,
+  `imagen` varchar(200) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `autor` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `genero` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libroLeido`
+--
+
+CREATE TABLE `libroLeido` (
+  `idLibroLeido` int(200) NOT NULL,
+  `idLibroFK` int(11) DEFAULT NULL,
+  `idUsuarioFK` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seguido`
+--
+
+CREATE TABLE `seguido` (
+  `idSeguido` int(200) NOT NULL,
+  `nombreSeguido` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `idUsuarioFK` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seguidor`
+--
+
+CREATE TABLE `seguidor` (
+  `idSeguidor` int(200) NOT NULL,
+  `nombreSeguidor` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `idUsuarioFK` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(200) NOT NULL,
+  `nombreUsuario` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `contraseña` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `correo` varchar(20) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  ADD PRIMARY KEY (`idCalificacion`),
+  ADD KEY `idComentarioFK` (`idComentarioFK`);
+
+--
+-- Indices de la tabla `calificacionTotal`
+--
+ALTER TABLE `calificacionTotal`
+  ADD PRIMARY KEY (`idCalificacionTotal`),
+  ADD KEY `idLibroFK` (`idLibroFK`);
+
+--
+-- Indices de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`idComentario`),
+  ADD KEY `idLibroFK` (`idLibroFK`),
+  ADD KEY `idUsuarioFK` (`idUsuarioFK`);
+
+--
+-- Indices de la tabla `libro`
+--
+ALTER TABLE `libro`
+  ADD PRIMARY KEY (`idLibro`);
+
+--
+-- Indices de la tabla `libroLeido`
+--
+ALTER TABLE `libroLeido`
+  ADD PRIMARY KEY (`idLibroLeido`),
+  ADD KEY `idLibroFK` (`idLibroFK`),
+  ADD KEY `idUsuarioFK` (`idUsuarioFK`);
+
+--
+-- Indices de la tabla `seguido`
+--
+ALTER TABLE `seguido`
+  ADD PRIMARY KEY (`idSeguido`),
+  ADD KEY `idUsuarioFK` (`idUsuarioFK`);
+
+--
+-- Indices de la tabla `seguidor`
+--
+ALTER TABLE `seguidor`
+  ADD PRIMARY KEY (`idSeguidor`),
+  ADD KEY `idUsuarioFK` (`idUsuarioFK`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  MODIFY `idCalificacion` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `calificacionTotal`
+--
+ALTER TABLE `calificacionTotal`
+  MODIFY `idCalificacionTotal` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `idComentario` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `libro`
+--
+ALTER TABLE `libro`
+  MODIFY `idLibro` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `libroLeido`
+--
+ALTER TABLE `libroLeido`
+  MODIFY `idLibroLeido` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `seguido`
+--
+ALTER TABLE `seguido`
+  MODIFY `idSeguido` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `seguidor`
+--
+ALTER TABLE `seguidor`
+  MODIFY `idSeguidor` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  ADD CONSTRAINT `calificacion_ibfk_1` FOREIGN KEY (`idComentarioFK`) REFERENCES `comentario` (`idComentario`);
+
+--
+-- Filtros para la tabla `calificacionTotal`
+--
+ALTER TABLE `calificacionTotal`
+  ADD CONSTRAINT `calificacionTotal_ibfk_1` FOREIGN KEY (`idLibroFK`) REFERENCES `libro` (`idLibro`);
+
+--
+-- Filtros para la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`idLibroFK`) REFERENCES `libro` (`idLibro`),
+  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`idUsuarioFK`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Filtros para la tabla `libroLeido`
+--
+ALTER TABLE `libroLeido`
+  ADD CONSTRAINT `libroLeido_ibfk_1` FOREIGN KEY (`idLibroFK`) REFERENCES `libro` (`idLibro`),
+  ADD CONSTRAINT `libroLeido_ibfk_2` FOREIGN KEY (`idUsuarioFK`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Filtros para la tabla `seguido`
+--
+ALTER TABLE `seguido`
+  ADD CONSTRAINT `seguido_ibfk_1` FOREIGN KEY (`idUsuarioFK`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Filtros para la tabla `seguidor`
+--
+ALTER TABLE `seguidor`
+  ADD CONSTRAINT `seguidor_ibfk_1` FOREIGN KEY (`idUsuarioFK`) REFERENCES `usuario` (`idUsuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
