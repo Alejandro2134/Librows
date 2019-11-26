a479a0ef7c9cfc2609fa694af695d36271bc6ae9
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2019-11-26 01:16:37 -0500

    Agregando rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+
 const pool = require('../database');
 
 router.get('/', (req, res) => {
     res.render('home');
+})
+
+router.post('/', passport.authenticate('local.auth', {
+    successRedirect: '/perfil',
+    failureRedirect: '/',
+    failureFlash: true
+}))
+
+router.get('/perfil', (req, res) => {
+    res.send('HOLA :D');
+})
+
+router.get('/catalogo', (req, res) => {
+    res.render('catalogo');
 })
 
 module.exports = router;
